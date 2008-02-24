fbc75424f678cd28cd0cca26231a44c5c4132606
zoolcar9 <zoolcar9@afe1a664-b246-0410-b0f8-f3ab68471790>
2008-02-24 21:21:51 +0000

    + Added preferences for tab opening

--- defaults/preferences/foxiewire_prefs.js
+++ defaults/preferences/foxiewire_prefs.js
@@ -1,2 +1,6 @@
 pref("extensions.foxiewire@foxiewire.com.description", "chrome://foxiewire/locale/foxiewire.properties");
+pref("extensions.FoxieWire.openInTab", true);
+pref("extensions.FoxieWire.openInTab.loadInBackground", 2);
+pref("extensions.FoxieWire.SplitBrowser.integrate", false);
+pref("extensions.FoxieWire.SplitBrowser.position", 8);
 
